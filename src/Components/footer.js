1e77ecef56b17c2a28dbe4f4e408e3023f4958bc
Pankaj Pundir <alvin@localhost.localdomain>
2021-01-02 22:44:40 +0530

    footer

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -5,64 +5,42 @@ import './footer.css'
 
 export default function footer() {
     return (
-        <footer md className="pt-1 container-fluid footerStyle " >
+        <footer md className=" container footerStyle " >
             <div className=" bg-dark">
             <div className="row">
-                <div className="col">
-                <a href="/Admission">Admission</a> 
+                <div className="col rightLink">
+                
+                <p><a href="/Admission">Admission</a> </p>
+                <p><a href="/aboutus">About Us</a> </p>
+                <p><a href="/contactus">Contact Us</a> </p>
+                
+
                 </div>
 
-                <div className="col">
-                <a href="/Admissionk">No Link</a> 
+                <div className="col rightLink">
+                <p><a href="/student-records">Student Records</a> </p>
+                <p><a href="/staff-records">Staff Records</a> </p>
+                <p><a href="/Add-records">Add Records</a> </p>
                 </div>
             </div>
-            <hr/>
-            <p className='last'>
+
+            <span className='last'>
+                <div className="social" >
+            <a href="https://www.facebook.com/mahavirinternationalschoolroorkee/" class="fa fa-facebook"></a>
+            <a href="https://www.instagram.com/misroorkee/" class="fa fa-instagram"></a>
+            <a href="https://www.youtube.com/channel/UCT0tDR0LZQf59UYLupDXZ9g" class="fa fa-youtube"></a>
+             
+                </div>
+            <p className="footerTail">
+
             Copyright © 2020 MAHAVIR INTERNATIONAL SCHOOL 
             </p>
+            </span>
             
             </div>
             
             
         </footer>
-
-    // <footer className="footer">
-    // <nav className="navbar navbar-default navbar-custom navbar-dark bg-dark">
-    //     <div className="container-fluid ">
-    //         <Row>
-    //                 <Col  className="flex-fill bg-success container-fluid">
-                        
-    //                         <ul >
-    //                             <li>
-    //                                 <a href="/Admission">Admission</a>
-    //                             </li>
-    //                     </ul>
-    //                 </Col>
-                    
-    //                 <Col className="flex-fill bg-danger container-fluid">
-                    
-    //                     <ul >
-    //                         <li>
-    //                             <a href="">Right link</a>
-    //                         </li>
-                            
-    //                     </ul>
-    //                 </Col>
-    //         </Row>
-            
-    //     <Row className="bg-info">
-    //         <div class="text-center">
-    //             <a href=""><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
-    //             <a href=""><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
-    //             <a href=""><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
-    //             <a href=""><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
-    //         </div>
-    //         </Row>
-    //     </div>
-
-    // </nav>
-    // </footer>
-
 
     )
 }
@@ -77,5 +55,6 @@ const footerStyle = {
     bottom: "0",
     height: 'auto',
     width: "100%",
-    color:'white'
+    color:'white',
+    justifyContent: 'center'
 }
