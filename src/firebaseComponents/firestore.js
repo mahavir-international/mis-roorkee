0de0544d38c584318daf960eeb057791db89cf3c
Pankaj Pundir <alvin@localhost.localdomain>
2021-01-02 19:43:54 +0530

    active now

--- src/firebaseComponents/firestore.js
+++ src/firebaseComponents/firestore.js
@@ -11,6 +11,18 @@ const firebaseConfig = {
     measurementId: "G-324XRDLLCM"
   };
 
-firebase.initializeApp(config);
+const firebaseConfiglearnFirebase = {
+    apiKey: "AIzaSyAwfriodGpGxNUOxXyFSsMnnEELvcjFdfU",
+    authDomain: "learnfirebase-c4b49.firebaseapp.com",
+    databaseURL: "https://learnfirebase-c4b49-default-rtdb.firebaseio.com",
+    projectId: "learnfirebase-c4b49",
+    storageBucket: "learnfirebase-c4b49.appspot.com",
+    messagingSenderId: "1016335262694",
+    appId: "1:1016335262694:web:91f84c7156ad3be69fec27"
+  };
+
+firebase.initializeApp(firebaseConfiglearnFirebase);
+
+// firebase.initializeApp(config);
 
 export default firebase;
